0c9db8eb57040e30d5b857ed23375b248b41b56c
Santhosh Kumar M <msanthoshkumar127@gmail.com>
2021-10-04 11:08:30 +0530

    Added Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import MultiStepForm from "./MultiStepForm"
+import MultiStepForm from "./MultiStepForm";
+import { BrowserRouter as Router } from "react-router-dom";
+import PersonalDetails from "./Component/PersonalDetails";
+import ProfessionalDetails from "./Component/ProfessionalDetails";
 
-function App(){
-    return(
-        <div className="App">
-            <MultiStepForm />
-        </div>
-    )
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        {/* <PersonalDetails /> */}
+        <ProfessionalDetails />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/MultiStepForm.tsx
+++ src/MultiStepForm.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+// import { useForm, useStep } from "react-hooks-helper";
+
+type PersonDetails = {
+  firstName: string;
+  lastName: string;
+  email: string;
+};
 
 const MultiStepForm = () => {
-    return (
-        <div>
-            <h1> Hello </h1>
-        </div>
-    )
-}
+  return (
+    <form>
+      <div className="field">
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" id="name" name="name" />
+      </div>
+
+      <div className="field">
+        <label htmlFor="lastName">Last Name</label>
+        <input type="number" id="score" name="score" />
+      </div>
+
+      <div className="field">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+      </div>
+      <button type="submit">Save</button>
+    </form>
+  );
+};
 
 export default MultiStepForm;
